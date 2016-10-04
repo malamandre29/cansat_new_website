6697ff76e42a876bdf04d941e302d3b00c64b02e
malamandre29 <nasioulas.v@gmail.com>
2016-10-04 11:14:15 +0300

    First commit

---
+++ new/admin/config.php
@@ -0,0 +1,14 @@
+<?php  
+// DB Settings 
+define('DB_SERVER', 'db53.grserver.gr:3306'); 
+define('DB_USER', 'chief'); 
+define('DB_PASSWORD', 'root_admin17'); 
+define('DB_NAME', 'eliasthe962377_');
+define('FROM_EMAIL', 'no_reply@ohyeahemail.com');
+define('FROM_NAME', 'oh yeah email!'); 
+session_start();
+require_once 'classes.php';
+$mini = false;
+$nonav = false;
+// error_reporting(0);
+?>
